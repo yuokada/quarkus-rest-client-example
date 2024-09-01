2e869c8bba364f3a9eac480c46004f88405a448d
Yukihiro Okada <yuokada@users.noreply.github.com>
2024-09-02 02:38:55 +0900

    Refactoring StubExampleEndpoint

--- server/src/main/java/io/github/yuokada/rest/service/StubExampleEndpoint.java
+++ server/src/main/java/io/github/yuokada/rest/service/StubExampleEndpoint.java
@@ -3,25 +3,15 @@ package io.github.yuokada.rest.service;
 import io.github.yuokada.rest.annotations.BasicAPI;
 import io.github.yuokada.rest.util.DummyDataGenerator;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.ws.rs.DefaultValue;
-import jakarta.ws.rs.HeaderParam;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Response;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
-import org.eclipse.microprofile.openapi.annotations.enums.Explode;
-import org.eclipse.microprofile.openapi.annotations.enums.ParameterIn;
-import org.eclipse.microprofile.openapi.annotations.enums.ParameterStyle;
-import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeIn;
 import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 import org.eclipse.microprofile.openapi.annotations.security.SecuritySchemes;
-import org.jboss.resteasy.reactive.Separator;
 
 @ApplicationScoped
 @Path("/api/v1")
@@ -58,21 +48,7 @@ public class StubExampleEndpoint implements ExampleEndpointApi {
     }
 
     @Override
-    public Response players(
-        @Parameter(schema = @Schema(type = SchemaType.NUMBER))
-        @QueryParam("offset") int offset,
-        @QueryParam("limit")
-        @Parameter(schema = @Schema(type = SchemaType.NUMBER))
-        @DefaultValue("50") int limit,
-        @Parameter(
-            in = ParameterIn.QUERY,
-            description = "filter with team ids",
-            style = ParameterStyle.FORM, explode = Explode.FALSE,
-            schema = @Schema(type = SchemaType.ARRAY, implementation = Integer.class)
-        )
-        @Separator(",")
-        @QueryParam("team_ids") Set<Integer> teamIds
-    ) {
+    public Response players(int offset, int limit, Set<Integer> teamIds) {
         Random random = new Random();
         System.out.println(teamIds);
         List<Player> players = DummyDataGenerator.getPlayers(random.nextInt(50), teamIds);
@@ -82,19 +58,7 @@ public class StubExampleEndpoint implements ExampleEndpointApi {
 
     // endpoints for guest users
     @Override
-    public Response testPlayers(
-        @Parameter(description = "test header parameter")
-        @DefaultValue("default-foo-value")
-        @HeaderParam("X-Foo") String fooHeader,
-        @Parameter(
-            in = ParameterIn.QUERY,
-            description = "Filter with team ids",
-            style = ParameterStyle.FORM, explode = Explode.FALSE
-            // , schema = @Schema(type = SchemaType.ARRAY, implementation = Integer.class)
-        )
-        @Separator(",")
-        @QueryParam("team_ids") Set<Integer> teamIds
-    ) {
+    public Response testPlayers(String fooHeader, Set<Integer> teamIds) {
         Random random = new Random();
         System.out.println(teamIds);
         List<Player> players = DummyDataGenerator.getPlayers(random.nextInt(50), teamIds);
